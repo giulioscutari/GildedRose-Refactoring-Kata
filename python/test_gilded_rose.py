1fd721883b8cc266149b1ec033a4e3118ba67b4d
Olly-didnt-work <scutarigiulio@gmail.com>
2023-07-18 11:56:29 +0200

    improve test coverage of update_quality

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,12 +5,28 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    """GildedRose tests.""""
+    
+    def test_quality_decreases(self):
+        """Test the quality of an object decreases."""
+        items = [Item("item", 10, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals(9, items[0].quality)
+    
+    def test_sellIn_decreases(self):
+        """Test the sellIn value of an object decreases."""
+        items = [Item("item", 10, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(9, items[0].sell_in)
 
+    def test_quality_increases(self):
+        """Test the quality of an object increases."""
+        items = [Item("Aged Brie", 10, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(11, items[0].quality)
         
 if __name__ == '__main__':
     unittest.main()
